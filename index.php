90bee91c0fa4dfa1784aa4b9586201e508230fdd
Willian Rocha <contato@ecorde.com.br>
2021-07-11 22:26:46 -0300

    Iniciando o ambiente para o TC002

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// Exibir todos os erros do PHP
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+// Incluir o composer apenas para algumas ferramentas do symfony
+	include_once('../vendor/autoload.php');
+
+
+
+dump('Estamos aqui!');
+
+
+
+
+
+?>
